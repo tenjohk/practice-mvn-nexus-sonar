ad62ff7dfc96ec6e0b96bd49692462a87f6e4824
tenjohk <tenjohk@yahoo.com>
2022-10-25 21:09:25 -0400

    fix sytax

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -1,20 +1,11 @@
 package com.example.demo;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class DemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-
-		String name ="Kelvin";
-
-		System.out.printIn(name);
-
-		Integer age = "20"
-		System.out.print(age + 5);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+        String name = "vamsi";
+        System.out.println(name);
+    }
 }
